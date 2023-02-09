afe34bd81e8b0c1704d2cd5e6203158f0b78f9ee
AlejandroMontalvo <alejandrormont@gmail.com>
2023-02-08 22:11:56 -0500

    Updated Styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 window.onload = function () {
   // Dark/Light Theme Logic
-  // Aqcuire the theme icons as variables
+  // Acquire the theme icons as variables
   let sunnyIcons = document.getElementsByName("sunny");
   let moonIcons = document.getElementsByName("moon");
   let themeSwitches = document.getElementsByClassName("switch");
 
-  /* Set the theme to the previously saved 
-     value for the theme within localstorage, if available  */
-  document.documentElement.setAttribute("data-theme", localStorage.theme);
+  /* Set the theme to the previously saved value for
+the theme within localstorage, if available */
+  let theme = localStorage.theme || "dark";
+  document.documentElement.setAttribute("data-theme", theme);
 
-  /* If the current theme is 'light', 
-     change the theme icon  */
-  if (localStorage.theme == "light") {
+  /* If the current theme is 'light',
+change the theme icon */
+  if (theme === "light") {
     for (let i = 0; i < sunnyIcons.length; i++) {
       sunnyIcons[i].style.display = "none";
       moonIcons[i].style.display = "inline-block";
@@ -20,48 +21,51 @@ window.onload = function () {
 
   for (let i = 0; i < themeSwitches.length; i++) {
     themeSwitches[i].addEventListener("click", function () {
-      if (localStorage.theme == "dark") {
+      if (theme === "dark") {
         for (let j = 0; j < moonIcons.length; j++) {
           sunnyIcons[j].style.display = "none";
           moonIcons[j].style.display = "inline-block";
         }
-        document.documentElement.setAttribute("data-theme", "light");
-        localStorage.theme = "light";
+        theme = "light";
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.theme = theme;
       } else {
         for (let j = 0; j < sunnyIcons.length; j++) {
           sunnyIcons[j].style.display = "inline-block";
           moonIcons[j].style.display = "none";
         }
-        document.documentElement.setAttribute("data-theme", "dark");
-        localStorage.theme = "dark";
+        theme = "dark";
+        document.documentElement.setAttribute("data-theme", theme);
+        localStorage.theme = theme;
       }
     });
   }
 
   let mobileLinks = document.getElementById("mobileNav__links");
   let menuIcon = document.getElementById("mobileNav__icon");
-  let menuClose = document.getElementsByClassName("mobileNav__close");
+  let menuClose = document.getElementsByClassName("mobileNav__close")[0];
 
-  menuIcon.addEventListener("click", function () {
+  menuIcon.addEventListener("click", toggleMenu);
+  menuClose.addEventListener("click", closeMenu);
+  mobileLinks.addEventListener("click", closeMenu);
+
+  function toggleMenu() {
     if (menuIcon.getAttribute("name") === "close-sharp") {
-      menuIcon.setAttribute("name", "menu-sharp");
-      mobileLinks.style.height = "0rem";
+      closeMenu();
     } else {
       menuIcon.setAttribute("name", "close-sharp");
-      mobileLinks.style.height = "13rem";
+      mobileLinks.style.height = "fit-content";
     }
-  });
+  }
 
-  for (let i = 0; i < menuClose.length; i++) {
-    menuClose[i].addEventListener("click", function () {
-      mobileLinks.style.height = "0rem";
-      menuIcon.setAttribute("name", "menu-sharp");
-    });
+  function closeMenu() {
+    mobileLinks.style.height = "0rem";
+    menuIcon.setAttribute("name", "menu-sharp");
   }
 
   // TypeWriter ---------------------------------------------------------------|
   // List of sentences
-  let content = ["create.", "design.", "develop."];
+  const content = ["create.", "design.", "develop."];
 
   // Current sentence being processed
   let contentIndex = 0;
@@ -125,54 +129,53 @@ window.onload = function () {
   // Start the typing effect on load
   _INTERVAL_VAL = setInterval(typeText, 100);
 
-  // let navButtonArray = document.querySelectorAll(".sideNav__button");
-  // let active = document.querySelector(
-  //   ".sideNav__button .sideNav__button--active"
-  // );
-  // for (let i = 0; i < navButtonArray.length; i++) {
-  //   navButtonArray[i].addEventListener("click", function () {
-  //     if (active) active.classList.remove("sideNav__button--active");
-  //     navButtonArray[i].classList.add("sideNav__button--active");
-  //   });
-  // }
+  // Get the links in the navbar
+  const navLinks = document.querySelectorAll(".nav__link");
 
-  // document.addEventListener("click", function (event) {
-  //   let active = document.querySelector(
-  //     ".sideNav__button .sideNav__button--active"
-  //   );
-  //   if (active) active.classList.remove("sideNav__button--active");
-  //   if (event.target.classList.contains("sideNav__button")) {
-  //     event.target.classList.remove("sideNav__button");
-  //     event.target.classList.add("sideNav__button--active");
-  //   }
-  // });
+  // Function to turn the background of a link red
+  const changeLinkBackground = (link) => {
+    navLinks.forEach((navLink) => {
+      navLink.style.color = "";
+      navLink.style.backgroundColor = "";
+    });
+    link.style.color = "var(--hover-font-color)";
+    link.style.backgroundColor = "var(--hover-color)";
+  };
 
-  // let homeSection = document.getElementById("home");
-  // let aboutSection = document.getElementById("about");
-  // let sideNavAbout = document.getElementById("sideNav__about");
-  // let projectsSection = document.getElementById("projects");
-  // let sideNavProjects = document.getElementById("sideNav__projects");
-  // console.log(aboutSection.offsetHeight);
-  // window.onscroll = function () {
-  //   if (window.scrollY < homeSection.offsetHeight / 1.5) {
-  //     sideNavAbout.style.background = "";
-  //     sideNavAbout.style.color = "";
-  //   } else if (
-  //     window.pageYOffset > homeSection.offsetHeight / 1.5 &&
-  //     window.pageYOffset < aboutSection.offsetTop - aboutSection.offsetHeight
-  //   ) {
-  //     sideNavAbout.style.background = "white";
-  //     sideNavAbout.style.color = "black";
-  //   } else if (
-  //     window.pageYOffset > aboutSection.offsetHeight / 2 &&
-  //     window.pageYOffset <
-  //       projectsSection.offsetTop + projectsSection.offsetHeight
-  //   ) {
-  //     sideNavProjects.style.background = "white";
-  //     sideNavProjects.style.color = "black";
-  //   } else {
-  //     sideNavAbout.style.background = "";
-  //     sideNavAbout.style.color = "";
-  //   }
-  // };
+  // Event listener for clicks on links
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      const sectionId = navLink.getAttribute("href");
+      const section = document.querySelector(sectionId);
+      section.scrollIntoView({ behavior: "smooth" });
+      changeLinkBackground(navLink);
+    });
+  });
+
+  // Event listener for scroll events
+  document.addEventListener("scroll", () => {
+    // Check if the user is within the first 500 pixels of the top of the page
+    if (window.scrollY < 600) {
+      navLinks.forEach((navLink) => {
+        navLink.style.color = "";
+        navLink.style.backgroundColor = "";
+      });
+    } else {
+      navLinks.forEach((navLink) => {
+        const sectionId = navLink.getAttribute("href");
+        const section = document.querySelector(sectionId);
+        const bounding = section.getBoundingClientRect();
+        const middle = (bounding.bottom - bounding.top) / 2;
+        if (
+          bounding.top + middle >= 0 &&
+          bounding.left >= 0 &&
+          bounding.right <= window.innerWidth &&
+          bounding.bottom - middle <= window.innerHeight
+        ) {
+          changeLinkBackground(navLink);
+        }
+      });
+    }
+  });
 };
